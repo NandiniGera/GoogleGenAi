5430a7a84ca92a735afca777aea2491c2cae5ad5
AbhinavDev2002 <abhinavagg2002@gmail.com>
2024-09-29 19:17:47 +0530

    Pushed Frontend

--- Frontend/src/Components/Services/services.jsx
+++ Frontend/src/Components/Services/services.jsx
@@ -7,7 +7,7 @@ function Services() {
   return (
 
     <div className="container">
-      <h1 className="title">OUR HealthCare OFFERINGS</h1>
+      <h1 className="title">Our Healthcare Offerings</h1>
       <p className="description">
       Our healthcare chatbot is an AI-driven solution designed to enhance patient care by offering instant medical assistance, appointment scheduling, and personalized health guidance. It streamlines healthcare interactions, making it easier for users to access vital information and support, improving the overall patient experience.
       </p>
